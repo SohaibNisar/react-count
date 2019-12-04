e4c43ce8d96e2e4a1384bef90f03b11476572fcd
Sohaib Nisar <sohaib39826@gmail.com>
2019-12-05 00:30:55 +0500

    Sohaib Nisar

--- app.js
+++ app.js
@@ -36,13 +36,13 @@ class App extends Component {
         return (
             <div className='container-fluid p-5 text-center'>
                 <div className='d-inline-block'>
-                    <div className='p-4 display'>
+                    <div className='p-4 display font-weight-bold'>
                         {this.state.count}
                     </div>
                     <div className='d-flex justify-content-center'>
-                        <button className='btn btn-success' onClick={this.countUp}><span className="fas fa-plus"></span></button>
-                        <button className='btn btn-info' onClick={this.reset}><span className="fas fa-redo"></span></button>
-                        <button className='btn btn-danger' onClick={this.countDown}><span className="fas fa-minus"></span></button>
+                        <button className='btn btn-success px-4 py-3 shadow-none rounded-0' onClick={this.countUp}><span className="fas fa-plus"></span></button>
+                        <button className='btn btn-info px-4 py-3 shadow-none rounded-0' onClick={this.reset}><span className="fas fa-redo"></span></button>
+                        <button className='btn btn-danger px-4 py-3 shadow-none rounded-0' onClick={this.countDown}><span className="fas fa-minus"></span></button>
                     </div>
                 </div>
             </div>
